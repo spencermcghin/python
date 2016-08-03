b013e74433fbdb7ed81190a223635a6ea5763d07
spencermcghin <spencer.mcghin@rittmanmead.com>
2016-08-03 12:51:55 -0700

    Update Battleship.py

--- Battleship.py
+++ Battleship.py
@@ -26,5 +26,10 @@ if guess_row == ship_row and guess_col == ship_col:
     print "Congratulations! You sank my battleship!"
 else:
     print "You missed my battleship!"
-board[guess_row][guess_col] = "X"
+if guess_row not in range(5) or guess_col not in range(5):
+    print "Oops, that's not even in the ocean."
+else:
+    print "You missed my battleship!"
+    board[guess_row][guess_col] = "X"
+    
 print_board(board)
