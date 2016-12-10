a2fa353318a4307a7147d0245a739d60d856b372
spencermcghin <spencer.mcghin@rittmanmead.com>
2016-12-10 14:37:44 -0800

    begin functional programming portion

--- python_data_science/data_science.py
+++ python_data_science/data_science.py
@@ -6,10 +6,26 @@ import pandas as pd
 
 """ Functions """
 
+# Configure data frame display output
+pd.set_option('display.max_rows', 500)
+pd.set_option('display.max_columns', 15)
+pd.set_option('display.width', 1000)
 
-def build_dataframe(file):
-    with open(file, 'rt') as csv_file:
-        data = pd.DataFrame(pd.read_csv(csv_file))
-        print(data)
 
-build_dataframe('/Users/SMcGhin/Documents/python/data_sets/vgsales.csv')
+# Create data frame object with which to work
+csv_file = open('/Users/SMcGhin/Documents/python/data_sets/vgsales.csv', 'rt')
+data = pd.DataFrame(pd.read_csv(csv_file))
+
+
+# Takes dim and fact column from data frame as input and outputs two columns
+# grouping by the dim, and sums the fact
+def sum_by_group(d, f):
+    group = data.groupby(d)[f].sum()
+    print(group)
+
+
+# Displays statistical, quartile information given two dims as inputs
+def describe_data_frame(dim1, dim2):
+    print(data.groupby(dim1)[dim2].describe().unstack())
+
+
