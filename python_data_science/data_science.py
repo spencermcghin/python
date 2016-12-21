4d4914f3f6ffae46322f7bc6cceff023dd2f7b38
spencermcghin <spencer.mcghin@rittmanmead.com>
2016-12-20 17:07:30 -0800

    finished up analyses for Game of Thrones dataset

--- python_data_science/data_science.py
+++ python_data_science/data_science.py
@@ -2,7 +2,6 @@
 
 """ Imports """
 
-import numpy as np
 import pandas as pd
 import matplotlib.pyplot as mp
 mp.style.use('fivethirtyeight')
@@ -22,8 +21,8 @@ pd.set_option('display.width', 1000)
 """ Functions """
 
 # Create data frame object with which to work
-csv_file = open('/Users/SMcGhin/Documents/python/data_sets/game-of-thrones/battles.csv', 'rt')
-data = pd.DataFrame(pd.read_csv(csv_file))
+csv_file_01 = open('/Users/SMcGhin/Documents/python/data_sets/game-of-thrones/battles.csv', 'rt')
+data = pd.DataFrame(pd.read_csv(csv_file_01))
 # print(data.head())
 
 
@@ -32,31 +31,76 @@ data = pd.DataFrame(pd.read_csv(csv_file))
 
 
 # % Battles Won by House
-house_battles = data[['attacker_1', 'attacker_outcome']]
-total_battles = pd.DataFrame(house_battles.groupby(['attacker_1']).count())
-total_battles.columns = ['total_battles']
-print(total_battles)
-wins = house_battles[house_battles['attacker_outcome'] == 'win']
-house_wins = pd.DataFrame(wins.groupby(['attacker_1']).count())
+house_battles = data[['attacker_1', 'attacker_outcome']].groupby(['attacker_1']).count()
+house_battles.columns = ['total_battles']
+print(house_battles)
+wins = data[['attacker_1', 'attacker_outcome']]
+house_wins = wins[wins['attacker_outcome'] == 'win'].groupby(['attacker_1']).count()
 house_wins.columns = ['battles_won']
-win_percent = pd.DataFrame((house_wins.battles_won/total_battles.total_battles) * 100)
+print(house_wins)
+win_percent = (house_wins.battles_won/house_battles.total_battles) * 100
 win_percent.columns = ['% Battles Won']
-wins_table = house_wins.join(total_battles)
-wins_table.plot(kind='bar')
+wins_table = house_wins.join(house_battles)
+wins_table.plot(kind='bar', grid=False, title='Battles Won to Fought')
 mp.show()
 
 
-
-
 # Won Battles Over Time
-# time_battles = data[['year', 'name', 'attacker_1', 'attacker_size']].fillna(0)
-# time_set_filter = time_battles[time_battles['attacker_1'] == 'Lannister']
-# troops = time_set_filter.groupby(['year', 'name']).sum()
-# print(time_battles.groupby(['year', 'attacker_1', 'name']).sum())
-# troops.unstack().plot(kind='bar', stacked=True, title='Lannister Troops by Battle')
-# mp.show()
+time_battles = data[['year', 'name', 'attacker_1', 'attacker_size']].fillna(0)
+time_set_filter = time_battles[time_battles['attacker_1'] == 'Lannister']
+troops = time_set_filter.groupby(['year', 'name']).sum()
+print(time_battles.groupby(['year', 'attacker_1', 'name']).sum())
+troops.unstack().plot(kind='bar', stacked=True, title='Lannister Troops by Battle')
+mp.show()
 
 
+"""
+
+Character Deaths Data Set
+
+"""
+
+# Create character death data frame object with which to work
+csv_file_02 = open('/Users/SMcGhin/Documents/python/data_sets/game-of-thrones/character-deaths.csv', 'rt')
+death_data = pd.DataFrame(pd.read_csv(csv_file_02))
+death_data['Allegiances'] = death_data['Allegiances'].str.replace('House ', '')  # Remove House prefix to group values
+# print(death_data.head())
+
+# Count character deaths by allegiance
+fil_char_deaths_01 = death_data[death_data['Allegiances'] != 'None']  # Filter out unwanted alignments
+fil_char_deaths_02 = fil_char_deaths_01[fil_char_deaths_01['Allegiances'] != 'Wildling']
+fil_char_deaths_03 = fil_char_deaths_02[fil_char_deaths_02['Allegiances'] != "Night's Watch"]
+char_deaths = fil_char_deaths_03[['Name', 'Allegiances']].groupby(['Allegiances']).count()
+char_deaths.columns = ['total_deaths']
+print(char_deaths)
+
+# Basic bar showing deaths by house
+char_deaths.sort_values(by='total_deaths')\
+    .plot(kind='barh', grid=False, legend=None, title='Character Deaths by House Alignment')
+mp.show()
+
+# Plot number of noble deaths per total deaths by house
+noble_deaths_filter = fil_char_deaths_03[fil_char_deaths_03['Nobility'] == 1]  # Build noble death column
+noble_table = noble_deaths_filter[['Name', 'Allegiances']].groupby(['Allegiances']).count()
+noble_table.columns = ['noble_deaths']
+noble_death_table = noble_table.join(char_deaths)  # Join to existing table to plot
+print(noble_death_table)
+noble_death_table.sort_values(by='total_deaths', ascending=False)\
+    .plot(kind='bar', grid=False, title='House Noble Deaths to Total')
+mp.show()
+
+# Get % of Deaths to Total Deaths
+death_perc = (char_deaths/char_deaths.sum()) * 100
+death_perc.columns = ['perc_tot_deaths']
+print(death_perc)
+
+# Donut Chart for % of Death Total
+death_perc.plot(kind='pie', subplots=True, legend=False, title='Proportion of All Named Deaths')
+center_circle = mp.Circle((0, 0), 0.75, color='black', fc='white', linewidth=1.25)  # Draw circle in center
+fig = mp.gcf()
+fig.gca().add_artist(center_circle)
+mp.axis('equal')  # Set aspect ratio to be equal so drawn as circle
+mp.show()
 
 
 
