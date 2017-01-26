aef3da2890b3e7f01da2e7cab2dd20308abdfa87
spencermcghin <spencer.mcghin@rittmanmead.com>
2017-01-26 15:25:08 -0800

    Added task code

--- RMTask/RM_Task.py
+++ RMTask/RM_Task.py
@@ -1,6 +1,4 @@
-""" Create an rmtask slash command for slack integration with JIRA. """
-
-# Imports
+"""Slack slash command for jira interaction."""
 from flask import Flask
 from flask_slack import Slack
 from slackclient import SlackClient
@@ -10,23 +8,53 @@ import json
 import os
 
 
-# Slack Variables
-SLACK_TOKEN = os.environ.get('SLACK_TOKEN')
-slack_client = SlackClient(SLACK_TOKEN)
+JIRA_URL = os.getenv('JIRA_URL')
+options = {
+    'server': JIRA_URL,
+}
+jira = JIRA(
+    options,
+    basic_auth=(
+        os.getenv('JIRA_USER'),
+        os.getenv('JIRA_PASSWORD')))
 
-
-# Jira Variables
-jira_uri = 'https://rittmanmead.atlassian.net/'
-
-
-# Flask
 app = Flask(__name__)
+slack = Slack(app)
+sc = SlackClient(os.getenv('SLACK_TOKEN'))
+app.add_url_rule('/', view_func=slack.dispatch)
 
 
-@app.route('/hello', methods=['POST'])
-def hello():
-    return 'Hello Slack!'
+"""Entry point for /jiratask. """
+
+
+@slack.command(
+    'jiratask',
+    token=os.getenv('SLACK_SLASH_TOKEN'),
+    team_id=os.getenv('SLACK_TEAM_ID'),
+    methods=['POST'])
+def __jira_handle(**kwargs):
+    user = get_jira_username_from_slack(kwargs.get('user_id'))
+    channel = kwargs.get('channel_id')
+    command = kwargs.get('text').partition(' ')[0].lower()
+    jira_key = kwargs.get('text').partition(' ')[2].partition(' ')[0].upper()
+    jira_data = kwargs.get('text').partition(' ')[2].partition(' ')[2]
+
+
+def get_jira_username_from_slack(user_id):
+    response = json.loads(sc.api_call('users.info', user=user_id))
+    if response.get("ok"):
+        slack_user = response['user']['profile']['email']
+        return get_jira_username(slack_user)
+    return ""
+
+
+def get_jira_username(user_id):
+    user = jira.search_users(user_id)
+    return user[0].name if len(user) == 1 else ''
+
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(debug=False, host='127.0.0.1', port=5000)
+
+
+
