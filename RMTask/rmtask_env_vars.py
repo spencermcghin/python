1fece0b854a83e980bdc187260456a59699c3125
spencermcghin <spencer.mcghin@rittmanmead.com>
2017-01-30 08:31:43 -0800

    Added internet programming dir

---
+++ RMTask/rmtask_env_vars.py
@@ -0,0 +1,13 @@
+import os
+
+os.environ['JIRA_URL'] = 'https://rittmanmead.atlassian.net'
+
+os.environ['JIRA_USER'] = 'spencer.mcghin'
+
+os.environ['JIRA_PASSWORD'] = 'BLADErunner1'
+
+os.environ['SLACK_TOKEN'] = 'cxoxp-2510066817-2543864854-133850972935-d17f532401c086cd5a98d05ef71a8775'
+
+os.environ['SLACK_TEAM_ID'] = 'Rittman Mead'
+
+os.environ['SLACK_SLASH_TOKEN'] = 'pTRXUfojRmYgZfEsccOQ6Hik'
