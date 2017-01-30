b30289d350c247e6eb162efadb40ca6b64c692b4
spencermcghin <spencer.mcghin@rittmanmead.com>
2017-01-30 08:28:08 -0800

    fixed auto domain home error catcher

---
+++ SMcGhin_IP/session03/wsgi-calc/calculator.py
@@ -0,0 +1,68 @@
+import re
+
+operations = {'add': add, 'subtract': subtract, 'multiply': multiply, 'divide': divide}
+
+
+def add(x, y):
+    return x + y
+
+
+def subtract(x, y):
+    return x - y
+
+
+def multiply(x, y):
+    return x * y
+
+
+def divide(x, y):
+    return x / y
+
+
+def equations():
+    body = ['<h1>Choose an operation:/h1>', '<ul>']
+    formula_template = '<li><a href="/{formula}">{formula}</a></li>'
+    for ops in operations:
+        body.append(formula_template.format(*ops))
+    body.append('</ul>')
+    return '\n'.join(body)
+
+
+def resolve_path(path):
+    urls = [(r'^$', equations)]
+    matchpath = path.lstrip('/')
+    for regexp, func in urls:
+        match = re.match(regexp, matchpath)
+        if match is None:
+            continue
+        args = match.groups([])
+        return func, args
+    # we get here if no url matches
+    raise NameError
+
+
+def application(environ, start_response):
+    headers = [("Content-type", "text/html")]
+    try:
+        path = environ.get('PATH_INFO', None)
+        if path is None:
+            raise NameError
+        func, args = resolve_path(path)
+        body = func(*args)
+        status = "200 OK"
+    except NameError:
+        status = "404 Not Found"
+        body = "<h1>Not Found</h1>"
+    except Exception:
+        status = "500 Internal Server Error"
+        body = "<h1>Internal Server Error</h1>"
+    finally:
+        headers.append(('Content-length', str(len(body))))
+        start_response(status, headers)
+        return [body.encode('utf8')]
+
+
+if __name__ == '__main__':
+    from wsgiref.simple_server import make_server
+    srv = make_server('localhost', 8080, application)
+    srv.serve_forever()
