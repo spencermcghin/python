b4b8d4931c0db30ad26ba0c903e1ee374ea69205
spencermcghin <spencer.mcghin@rittmanmead.com>
2017-02-01 14:32:53 -0800

    Combined in RMTask

--- RMTask/RM_Task.py
+++ RMTask/RM_Task.py
@@ -1,23 +1,5 @@
-
-# Imports
-from flask import *
-from slackclient import SlackClient
-from jira import JIRA
-
-
-# JIRA Variables
-user = 'spencer.mcghin@rittmanmead.com'
-password = 'BLADErunner1'
-server = 'https://rittmanmead.atlassian.net'
-options = {'server': server}
-jira = JIRA(options=options, basic_auth=(user, password))
-
-# Slack Variables
-slack_client = SlackClient('xoxp-2510066817-2543864854-133850972935-d17f532401c086cd5a98d05ef71a8775')
-
-
 # The parameters included in a slash command request (with example values):
-#   token=gIkuvaNzQIHg97ATvDxqgjtO
+#   token=gIkuvaNzQIHg97ATvDxqgjt
 #   team_id=T0001
 #   team_domain=example
 #   channel_id=C2147483705
@@ -27,6 +9,28 @@ slack_client = SlackClient('xoxp-2510066817-2543864854-133850972935-d17f532401c0
 #   command=/weather
 #   text=94070
 #   response_url=https://hooks.slack.com/commands/1234/5678
+
+
+# Imports
+from flask import *
+from slackclient import SlackClient
+from jira import JIRA
+
+# JIRA Variables
+jira_user = 'rm.api'
+password = 'FIRST_MONTH_RAIN'
+server = 'https://rittmanmead.atlassian.net'
+options = {'server': server}
+jira = JIRA(options=options, basic_auth=(jira_user, password))
+
+# Slack Variables
+slack_client = SlackClient('xoxp-2510066817-2543864854-133850972935-d17f532401c086cd5a98d05ef71a8775')
+slack_user = request.form.get('user_name', None)
+token = request.form.get('token', None)
+text = request.form.get('text', None)
+project = text.split(' ')[0]
+description = text.split(' ')[1]
+user_name = request.form.get('user_name', None)
 
 # Flask Variables
 app = Flask(__name__)
@@ -45,10 +49,11 @@ def connect_jira(jira_server, jira_user, jira_password):
 def jira_issue():
     try:
         issue_dict = {
-            "project": {"key": 'SM'},
+            "project": {"key": project},
             "summary": "something's wrong",
             "description": "test",
-            "issuetype": {"name": "Task"}
+            "issuetype": {"name": "Task"},
+            "assignee": ''
         }
         new_issue = jira.create_issue(fields=issue_dict)
         return new_issue
@@ -57,15 +62,13 @@ def jira_issue():
         return None
 
 
+# def jira_project_list():
+#     try:
+
+
 # Function to get args from slack
 @app.route("/jiratask", methods=['POST'])
 def slack_response():
-    token = request.form.get('token', None)
-    text = request.form.get('text', None)
-    project = text.split(' ')[0]
-    description = text.split(' ')[1]
-    user_name = request.form.get('user_name', None)
-
     if not token:
         abort(400)
 
@@ -77,7 +80,7 @@ def slack_response():
             {
                 'fallback': 'Required plain-text summary of the attachment.',
                 'color': '#36a64f',
-                'author_name': user_name,
+                'author_name': jira_user,
                 'author_icon': 'https://www.atlassian.com/docroot/wac/resources/wac/img/social-icons/jira_logo.jpg',
                 'title': 'JIRA Task Created - {0}'.format(description),
                 'title_link': 'https://rittmanmead.atlassian.net',
