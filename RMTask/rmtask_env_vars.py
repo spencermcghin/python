51919fa716e0845d2daa7fca82fe80248e84a537
spencermcghin <spencer.mcghin@rittmanmead.com>
2017-05-18 08:29:56 -0700

    changed vars

--- RMTask/rmtask_env_vars.py
+++ RMTask/rmtask_env_vars.py
@@ -4,7 +4,7 @@ os.environ['JIRA_URL'] = 'https://rittmanmead.atlassian.net'
 
 os.environ['JIRA_USER'] = 'spencer.mcghin'
 
-os.environ['JIRA_PASSWORD'] = 'BLADErunner1'
+os.environ['JIRA_PASSWORD'] = ''
 
 os.environ['SLACK_TOKEN'] = 'cxoxp-2510066817-2543864854-133850972935-d17f532401c086cd5a98d05ef71a8775'
 
